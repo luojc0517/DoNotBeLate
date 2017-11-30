2ac91c3886312fc735323e8ed5e9c423eed66595
luojc0517 <luojiachen1@sina.com>
2017-11-30 11:41:03 +0800

    修改布局文件

--- client/pages/index/routes.js
+++ client/pages/index/routes.js
@@ -2,22 +2,22 @@ var PageItems =
   [
     {
       text: '聚餐',
-      icon: 'user-unlogin.png',
+      icon: '../../images/选择类型/u10.png',
       route: '../c1/c1',
     },
     {
       text: '运动',
-      icon: 'user-unlogin.png',
+      icon: '../../images/选择类型/u28.png',
       route: '../c2/c2',
     },
     {
       text: '休闲',
-      icon: 'user-unlogin.png',
+      icon: '../../images/选择类型/u38.png',
       route: '../c3/c3',
     },
     {
       text: '必到',
-      icon: 'user-unlogin.png',
+      icon: '../../images/选择类型/u48.png',
       route: '../c4/c4',
     }
   ];
