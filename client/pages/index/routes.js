adb114ec65a29d0f908e3fff652a232f1198b882
luojc0517 <luojiachen1@sina.com>
2017-12-07 14:23:53 +0800

    1.结果页UI还原，使用假数据填充 2.替换切图为@2x

--- client/pages/index/routes.js
+++ client/pages/index/routes.js
@@ -3,8 +3,8 @@ var PageItems =
     {
       iIndex: 0,
       text: '聚餐',
-      icon: '../../images/img_party.png',
-      selIcon:'../../images/img_party.png',
+      icon: '../../images/img_party@2x.png',
+      selIcon:'../../images/img_party@2x.png',
       selected: false,
       maxLate: 15,
       bkgClass:"pages-icon-wrapper-party"
@@ -12,8 +12,8 @@ var PageItems =
     {
       iIndex: 1,
       text: '运动',
-      icon: '../../images/img_sport.png',
-      selIcon: '../../images/img_sport.png',
+      icon: '../../images/img_sport@2x.png',
+      selIcon: '../../images/img_sport@2x.png',
       selected: false,
       maxLate: 30,
       bkgClass: "pages-icon-wrapper-sport"
@@ -21,8 +21,8 @@ var PageItems =
     {
       iIndex: 2,
       text: '休闲',
-      icon: '../../images/img_hangout.png',
-      selIcon: '../../images/img_hangout.png',
+      icon: '../../images/img_hangout@2x.png',
+      selIcon: '../../images/img_hangout@2x.png',
       selected: false,
       maxLate: 40,
       bkgClass: "pages-icon-wrapper-hangout"
@@ -30,8 +30,8 @@ var PageItems =
     {
       iIndex: 3,
       text: '必到',
-      icon: '../../images/img_important.png',
-      selIcon: '../../images/img_important.png',
+      icon: '../../images/img_important@2x.png',
+      selIcon: '../../images/img_important@2x.png',
       selected: false,
       maxLate: 0,
       bkgClass: "pages-icon-wrapper-important"
--- client/pages/result/result.js
+++ client/pages/result/result.js
@@ -1,7 +1,28 @@
 Page({
   data: {
-    lateInMin:0,
-    directDis:0
+    lateInMin: 0,
+    directDis: 0,
+    totalFine: "¥12.78",
+    pageItems: [{
+      title: "出发地点",
+      text: "高新园3道XXX大厦"
+    }, {
+      title: "活动地点",
+      text: "万象天地"
+    }, {
+      title: "活动时间",
+      text: "2017-12-05  12:20"
+    }, {
+      title: "到达时间",
+      text: "2017-12-05  13:04"
+    }, {
+      title: "距离间隔",
+      text: "3.2 km"
+    }, {
+      title: "时间间隔",
+      text: "24 min"
+    }]
+
   },
   onLoad: function () {
     var that = this;
@@ -17,7 +38,7 @@ Page({
 
     wx.getStorage({
       key: 'directDis',
-      success: function(res) {
+      success: function (res) {
         console.log(res.data);
         that.setData({
           directDis: res.data
